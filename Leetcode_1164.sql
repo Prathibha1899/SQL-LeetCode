5877d5c81afcdd0a47fa6ef0383be0a79fbf53e8
Prathibha1899 <prathibhavuyyala1899@gmail.com>
2025-03-28 15:52:59 -0400

    commit_17

---
+++ Leetcode_1164.sql
@@ -0,0 +1,54 @@
+"""
+Table: Products
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| new_price     | int     |
+| change_date   | date    |
++---------------+---------+
+(product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+Each row of this table indicates that the price of some product was changed to a new price at some date.
+ 
+
+Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output: 
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+"""
+with temp as (select product_id,new_price,rank() over(partition by product_id order by change_date desc) as rnk from Products where change_date<='2019-08-16')
+,t as (
+    select * from temp where rnk=1
+)
+, temp2 as (
+     select distinct product_id from Products
+)
+
+select temp2.product_id, coalesce(t.new_price,10) as price from temp2 left join t on temp2.product_id=t.product_id ;
---
+++ Leetcode_180.sql
@@ -0,0 +1,48 @@
+"""
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+"""
+with temp as (
+select num, lag(num,1) over(order by id) as lag_num, lead(num,1) over(order by id) as lead_num from Logs)
+
+select distinct num as ConsecutiveNums  from temp where num=lag_num and num=lead_num;
